ee0c176e09f0032efc90743c5bf89f93fc80f94c
Quima Ruano <jruanomed@gmail.com>
2022-05-07 07:26:48 +0200

    #commit1

---
+++ src/main/java/com/example/demoGridsv2/FileController.java
@@ -0,0 +1,43 @@
+package com.example.demoGridsv2;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("file")
+public class FileController {
+
+    @Autowired
+    private FileService fileService;
+    //se guardan en AnalaitycDb.fs.files
+    @PostMapping("/upload")//http://localhost:8080/file/upload?file     Seleccionar Body-->file-->file
+    public ResponseEntity<?> upload(@RequestParam("file")MultipartFile file) throws IOException {
+        return new ResponseEntity<>(fileService.addFile(file), HttpStatus.OK);
+    }
+
+    @GetMapping("/download/{id}")//http://localhost:8080/file/download/6270d071b29c6404528a9db2
+    public ResponseEntity<ByteArrayResource> download(@PathVariable String id) throws IOException {
+        LoadFile loadFile = fileService.downloadFile(id);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(loadFile.getFileType() ))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + loadFile.getFilename() + "\"")
+                .body(new ByteArrayResource(loadFile.getFile()));
+    }
+
+}
---
+++ src/test/java/com/example/demoGridsv2/DemoGridsv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoGridsv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGridsv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
